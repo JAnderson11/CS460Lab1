69efa17cec88d17b0d507b6373d96b42db06390a
JAnderson11 <jacobanderson1104@gmail.com>
2017-09-27 18:30:02 -0600

    finished first 2 parts

---
+++ lab1/two-nodes.py
@@ -0,0 +1,102 @@
+from __future__ import print_function
+
+import sys
+
+sys.path.append('..')
+
+from src.sim import Sim
+from src.packet import Packet
+
+from networks.network import Network
+
+
+class DelayHandler(object):
+    @staticmethod
+    def receive_packet(packet):
+        print((Sim.scheduler.current_time(),
+               packet.ident,
+               packet.created,
+               Sim.scheduler.current_time() - packet.created,
+               packet.transmission_delay,
+               packet.propagation_delay,
+               packet.queueing_delay))
+
+
+def main():
+    #===================PART 1=====================
+    # parameters
+    Sim.scheduler.reset()
+
+    # setup network
+    net = Network('networks/part1.txt')
+
+    # setup routes
+    n1 = net.get_node('n1')
+    n2 = net.get_node('n2')
+    n1.add_forwarding_entry(address=n2.get_address('n1'), link=n1.links[0])
+    # n2.add_forwarding_entry(address=n1.get_address('n2'), link=n2.links[0])
+
+    # setup app
+    d = DelayHandler()
+    net.nodes['n2'].add_protocol(protocol="delay", handler=d)
+
+    # send one packet
+    p = Packet(destination_address=n2.get_address('n1'), ident=1, protocol='delay', length=1000)
+    Sim.scheduler.add(delay=0, event=p, handler=n1.send_packet)
+
+    # run the simulation
+    Sim.scheduler.run()
+
+        #===================PART 2=====================
+    # parameters
+    Sim.scheduler.reset()
+
+    # setup network
+    net = Network('networks/part2.txt')
+
+    # setup routes
+    n1 = net.get_node('n1')
+    n2 = net.get_node('n2')
+    n1.add_forwarding_entry(address=n2.get_address('n1'), link=n1.links[0])
+    #n2.add_forwarding_entry(address=n1.get_address('n2'), link=n2.links[0])
+
+    # setup app
+    d = DelayHandler()
+    net.nodes['n2'].add_protocol(protocol="delay", handler=d)
+
+    # send one packet
+    p = Packet(destination_address=n2.get_address('n1'), ident=1, protocol='delay', length=1000)
+    Sim.scheduler.add(delay=0, event=p, handler=n1.send_packet)
+
+    # run the simulation
+    Sim.scheduler.run()
+
+            #===================PART 3=====================
+    # parameters
+    Sim.scheduler.reset()
+
+    # setup network
+    net = Network('networks/part3.txt')
+
+    # setup routes
+    n1 = net.get_node('n1')
+    n2 = net.get_node('n2')
+    n1.add_forwarding_entry(address=n2.get_address('n1'), link=n1.links[0])
+    #n2.add_forwarding_entry(address=n1.get_address('n2'), link=n2.links[0])
+
+    # setup app
+    d = DelayHandler()
+    net.nodes['n2'].add_protocol(protocol="delay", handler=d)
+
+    # send one packet
+    p = Packet(destination_address=n2.get_address('n1'), ident=1, protocol='delay', length=1000)
+    Sim.scheduler.add(delay=0, event=p, handler=n1.send_packet)
+    Sim.scheduler.add(delay=0, event=p, handler=n1.send_packet)
+    Sim.scheduler.add(delay=0, event=p, handler=n1.send_packet)
+    Sim.scheduler.add(delay=2, event=p, handler=n1.send_packet)
+
+    # run the simulation
+    Sim.scheduler.run()
+
+if __name__ == '__main__':
+    main()
